a48e4300c795a422390bbc26ca8799e1dbe18b46
max <max.0x71@gmail.com>
2021-03-21 20:42:34 +0000

    hex text changes color depending on background

--- script/events.js
+++ script/events.js
@@ -4,6 +4,23 @@ function bindSavedEvent() {
 
         const id = event.currentTarget.id
 
+        const rgb = parseInt(id, 16);   // convert rrggbb to decimal
+        const r = (rgb >> 16) & 0xff;  // extract red
+        const g = (rgb >>  8) & 0xff;  // extract green
+        const b = (rgb >>  0) & 0xff;  // extract blue
+
+        const luma = 0.2126 * r + 0.7152 * g + 0.0722 * b; // per ITU-R BT.709
+
+        console.log(luma)
+
+        if (luma < 40) {
+            $(`#${id}-label`).css("color", "white")
+            console.log("white")
+        } else {
+            $(`#${id}-label`).css("color", "black")
+            console.log("black")
+        }
+
         $(`#${id}-label`).css("opacity", "100%")
     }, event => {
         event.preventDefault()
@@ -20,6 +37,23 @@ function bindHistoryEvent() {
 
         const id = event.currentTarget.id
 
+        const rgb = parseInt(id, 16);   // convert rrggbb to decimal
+        const r = (rgb >> 16) & 0xff;  // extract red
+        const g = (rgb >>  8) & 0xff;  // extract green
+        const b = (rgb >>  0) & 0xff;  // extract blue
+
+        const luma = 0.2126 * r + 0.7152 * g + 0.0722 * b; // per ITU-R BT.709
+
+        console.log(luma)
+
+        if (luma < 40) {
+            $(`#${id}-label`).css("color", "white")
+            console.log("white")
+        } else {
+            $(`#${id}-label`).css("color", "black")
+            console.log("black")
+        }
+
         $(`#${id}-label`).css("opacity", "100%")
     }, event => {
         event.preventDefault()
@@ -35,6 +69,23 @@ function bindPaletteEvent() {
         event.preventDefault()
 
         const id = event.currentTarget.id
+
+        const rgb = parseInt(id, 16);   // convert rrggbb to decimal
+        const r = (rgb >> 16) & 0xff;  // extract red
+        const g = (rgb >>  8) & 0xff;  // extract green
+        const b = (rgb >>  0) & 0xff;  // extract blue
+
+        const luma = 0.2126 * r + 0.7152 * g + 0.0722 * b; // per ITU-R BT.709
+
+        console.log(luma)
+
+        if (luma < 40) {
+            $(`#${id}-label`).css("color", "white")
+            console.log("white")
+        } else {
+            $(`#${id}-label`).css("color", "black")
+            console.log("black")
+        }
 
         $(`#${id}-label`).css("opacity", "100%")
     }, event => {
